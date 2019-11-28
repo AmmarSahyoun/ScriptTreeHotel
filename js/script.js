5c2eb7a732a3ec447b04a0947ab1f6c2bc8e7c6f
Ammar Sahyoun <ammarzgr@hotmail.com>
2019-11-28 20:49:19 +0100

    andra utveckling

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+class person {
+  constructor(username, email, phone) {
+    this.username = username;
+    this.email = email;
+    this.phone = phone;
+  }
+}
 
-class HotelGuest {
-  constructor() {
-    this.username = '';
-    this.email = '';
-    this.phone = '';
-    this.checkin = '';
-    this.checkout = '';
-    this.room = 'Single Room';
+class guest extends person {
+  constructor(username, email, phone) {
+    super(username, email, phone);
+    this.memberId = '';
+  }
+}
+
+class GuestBooking {
+  constructor(username, email, phone, memberId, checkin, checkout, room) {
+    this.guest = new guest(username, email, phone);
+    this.guest.memberId = memberId;
+    this.checkin = checkin;
+    this.checkout = checkout;
+    this.room = room;
   }
 }
 
@@ -17,28 +30,24 @@ function submitForm() {
   var usernameControl = document.getElementById('username');
   var emailControl = document.getElementById('email');
   var phoneControl = document.getElementById('phone');
+  var memberIdControl = document.getElementById('memberId');
   var checkinControl = document.getElementById('checkin');
   var checkoutControl = document.getElementById('checkout');
   var roomControl = document.getElementById('room');
 
-
-  var myGuest = new HotelGuest();
-
-  myGuest.username = usernameControl.value;
-  myGuest.email = emailControl.value;
-  myGuest.phone = phoneControl.value;
-  myGuest.checkin = checkinControl.value;
-  myGuest.checkout = checkoutControl.value;
-  myGuest.room = roomControl.value;
+  var myGuest = new GuestBooking(usernameControl.value
+    , emailControl.value, phoneControl.value, memberIdControl.value
+    , checkinControl.value, checkoutControl.value, roomControl.value);
 
   guests.push(myGuest);
 
   usernameControl.value = '';
   emailControl.value = '';
   phoneControl.value = '';
+  memberIdControl.value = '';
   checkinControl.value = '';
   checkoutControl.value = '';
-  roomControl.value = '';
+  roomControl.value = 'sn';
 
   alert('Guest added successfully');
 }
@@ -47,20 +56,26 @@ function populateGuestList() {
 
   var dataTableElement = document.getElementById('dataTableBody');
 
+  clearTable();
+
   for (index = 0; index < guests.length; index++) {
 
     var newRow = document.createElement("tr");
 
     var newCell = document.createElement("td");
-    newCell.innerHTML = guests[index].username;
+    newCell.innerHTML = guests[index].guest.username;
     newRow.append(newCell);
 
     newCell = document.createElement("td");
-    newCell.innerHTML = guests[index].email;
+    newCell.innerHTML = guests[index].guest.email;
     newRow.append(newCell);
 
     newCell = document.createElement("td");
-    newCell.innerHTML = guests[index].phone;
+    newCell.innerHTML = guests[index].guest.phone;
+    newRow.append(newCell);
+
+    newCell = document.createElement("td");
+    newCell.innerHTML = guests[index].guest.memberId;
     newRow.append(newCell);
 
     newCell = document.createElement("td");
@@ -76,6 +91,16 @@ function populateGuestList() {
     newRow.append(newCell);
 
     dataTableElement.append(newRow);
+
+  }
+
+  function clearTable() {
+    var tableControl = document.getElementById('dataTableBody');
+    var varRows = tableControl.rows.length;
+
+    for (var index = varRows - 1; index > -1; index--) {
+      tableControl.deleteRow(index);
+    }
 
   }
 }
